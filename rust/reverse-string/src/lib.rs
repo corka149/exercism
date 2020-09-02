05c3d84b6bcdc591293dbb62199440e9b88137ba
corka149 <corka149@mailbox.org>
2020-09-02 21:37:29 +0200

    Added rust track

---
+++ rust/reverse-string/src/lib.rs
@@ -0,0 +1,7 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub fn reverse(input: &str) -> String {
+    let mut g = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
+    g.reverse();
+    g.join("")
+}

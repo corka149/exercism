dca768871be083e88664fd63bc40506d178fe863
corka149 <corka149@mailbox.org>
2020-09-04 10:40:34 +0200

    Improved solution

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,7 @@
 use unicode_segmentation::UnicodeSegmentation;
+use std::iter::FromIterator;
 
 pub fn reverse(input: &str) -> String {
-    let mut g = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
-    g.reverse();
-    g.join("")
+    let mut g = UnicodeSegmentation::graphemes(input, true);
+    String::from_iter(g.rev())
 }

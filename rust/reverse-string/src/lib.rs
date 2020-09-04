70fa1550935b142229c49a68d3790b6f2e2a2f48
corka149 <corka149@mailbox.org>
2020-09-04 19:26:58 +0200

    Improved solution of reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
-use std::iter::FromIterator;
 
 pub fn reverse(input: &str) -> String {
-    let mut g = UnicodeSegmentation::graphemes(input, true);
-    String::from_iter(g.rev())
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
 }
